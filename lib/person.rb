df84b9305df3646a72dbf5410cd2e953522b15b8
Saung Oo Myat <saung.myat@gmail.com>
2022-06-10 14:55:32 -0400

    commit tree e8cb29bcf328f583a7f977dc6a97542d71faefb3

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
 
-end
+
+    def name
+      @name
+    end
+  
+    def name=(name)
+      @name = name
+    end
+  
+    def job
+      @job
+    end
+  
+    def job=(job)
+      @job = job
+    end
+
+    
+  end
